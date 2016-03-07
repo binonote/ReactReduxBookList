5a22ee19ffc0d7b9a8282a1a98712e2dabb1506e
Bao Dinh <bao.dinh@livenation.com>
2016-03-06 23:18:17 -0800

    -Remove apply middleware from index.js -Rewrite app.js to be function component

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Component } from 'react';
 import BookList from '../container/book-list';
 import BookDetail from '../container/book-detail';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <BookList />
-        <BookDetail />
-      </div>
-    );
-  }
+export default() => {
+  return (
+    <div>
+      <BookList />
+      <BookDetail />
+    </div>
+  );
 }
